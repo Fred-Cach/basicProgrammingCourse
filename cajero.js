729f3151503fad9bb475ea226eeeed30b4872942
Fred-Cach <federicocachero@hotmail.com>
2021-08-28 01:11:53 -0300

    closure added

--- cajero.js
+++ cajero.js
@@ -23,46 +23,51 @@ class Billete {
   }
 }
 
-const caja = [];
-caja.push(new Billete(100, 11));
-caja.push(new Billete(50, 10));
-caja.push(new Billete(20, 13));
-caja.push(new Billete(10, 14));
+const atm = () => {
+  const caja = [];
+  caja.push(new Billete(100, 11));
+  caja.push(new Billete(50, 10));
+  caja.push(new Billete(20, 13));
+  caja.push(new Billete(10, 14));
 
-let cajaDisponible = caja.reduce((a, b) => a + b.valor * b.cantidad, 0);
-cajero.innerHTML = `Monto disponible: ${cajaDisponible}`;
-
-const extraerDinero = () => {
-  const aEntregar = [];
-  let retiro = parseInt(dinero.value);
-
-  if (!(retiro > cajaDisponible)) {
-    for (b of caja) {
-      let div;
-      let papeles;
-      if (retiro) {
-        div = Math.floor(retiro / b.valor);
-        div >= b.cantidad ? (papeles = b.cantidad) : (papeles = div);
-        papeles ? aEntregar.push(new Billete(b.valor, papeles)) : null;
-        b.cantidad -= papeles;
-        retiro -= b.valor * papeles;
-      }
-    }
-
-    if (!retiro) {
-      for (let bill of aEntregar) {
-        console.log(bill.cantidad);
-        bill.showBill();
-      }
-      cajaDisponible -= dinero.value;
-    } else {
-      window.alert("El cajero no cuenta con el cambio disponible.");
-    }
-  } else {
-    window.alert("El cajero no cuenta con suficiente dinero disponible.");
-  }
+  let cajaDisponible = caja.reduce((a, b) => a + b.valor * b.cantidad, 0);
   cajero.innerHTML = `Monto disponible: ${cajaDisponible}`;
-  dinero.value = null;
+
+  const extraerDinero = () => {
+    const aEntregar = [];
+    let retiro = parseInt(dinero.value);
+
+    if (!(retiro > cajaDisponible)) {
+      for (b of caja) {
+        let div;
+        let papeles;
+        if (retiro) {
+          div = Math.floor(retiro / b.valor);
+          div >= b.cantidad ? (papeles = b.cantidad) : (papeles = div);
+          papeles ? aEntregar.push(new Billete(b.valor, papeles)) : null;
+          b.cantidad -= papeles;
+          retiro -= b.valor * papeles;
+        }
+      }
+
+      if (!retiro) {
+        for (let bill of aEntregar) {
+          console.log(bill.cantidad);
+          bill.showBill();
+        }
+        cajaDisponible -= dinero.value;
+      } else {
+        window.alert("El cajero no cuenta con el cambio disponible.");
+      }
+    } else {
+      window.alert("El cajero no cuenta con suficiente dinero disponible.");
+    }
+    cajero.innerHTML = `Monto disponible: ${cajaDisponible}`;
+    dinero.value = null;
+  };
+  return extraerDinero;
 };
 
-extraer.addEventListener("click", extraerDinero);
+const withdraw = atm();
+
+extraer.addEventListener("click", withdraw);
